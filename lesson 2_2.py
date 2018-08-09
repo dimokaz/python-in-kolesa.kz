24a9d4b804c03488bfbc98b24bab2130e8832065
dimokaz <42197030+dimokaz@users.noreply.github.com>
2018-08-09 21:21:21 +0600

    Update lesson 2_2.py

--- lesson 2_2.py
+++ lesson 2_2.py
@@ -9,8 +9,9 @@ age = input('Напишите Ваш возраст', )
 adress = input('Напишите Ваш адрес', )
 tel = input('Напишите Ваш телефон', )
 
-template = 'Имя: {name} \t Фамилия: {surname} \t Возраст: {age} \t Адрес: {adress} \t Телефон:{tel}'
-template.format(name = name, surname = surname, age = age, adress = adress, tel = tel)
+template = 'Имя: {name} \n Фамилия: {surname} \n Возраст: {age} \n Адрес: {adress} \n Телефон:{tel}'
+
+print(template.format(name = name, surname = surname, age = age, adress = adress, tel = tel))
 
 
 
